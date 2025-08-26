71a6e4d69fdc39db02ca44d210bf285ce2d19c35
ayandip <ayandip.paul@weavers-web.com>
2025-08-26 13:27:30 +0530

    implement subtract function and create test suite for that

--- math.test.ts
+++ math.test.ts
@@ -1,5 +1,10 @@
-import { add } from './math'
+import { add, subtract } from './math'
 
 test('1 and 2 should Equal to 3', () => {
     expect(add(1, 2)).toBe(3)
+})
+
+
+test('3 subtracted by 6 should return -3', () => {
+    expect(subtract(3, 6)).toBe(-3)
 })
--- math.ts
+++ math.ts
@@ -2,4 +2,9 @@ export function add(a: number, b: number): number {
     return a + b
 };
 
+export function subtract(b: number, c: number): number {
+    return b - c
+}
+
 add(1, 2)
+// subtract(3, 6)
