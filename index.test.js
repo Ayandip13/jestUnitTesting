9730d034716509e4c789c8c06184a83edd1e5d19
ayandip <ayandip.paul@weavers-web.com>
2025-08-26 12:08:40 +0530

    change run script and test a multiply fn

--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
 const multiply = require("./index");
 
 // test suit
-test("2 and 5 multiply should return 10");
+test("2 and 5 multiply should return 10", () => {
+    expect(multiply(2, 5)).toBe(10);
+});
