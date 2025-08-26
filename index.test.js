4a26f7507c7c0b04d5aed77f678f95ddcb11099a
ayandip <ayandip.paul@weavers-web.com>
2025-08-26 12:31:22 +0530

    add a bit of comment for better readabillity

--- index.test.js
+++ index.test.js
@@ -2,5 +2,12 @@ const multiply = require("./index");
 
 // test suit
 test("2 and 5 multiply should return 10", () => {
-    expect(multiply(2, 5)).toBe(10);
+    expect(multiply(2, 5)).toBe(10)
 });
+
+
+// expect(value) → assertion wrapper.
+// .toBe() → primitive equality.
+// .toEqual() → deep equality (objects/arrays).
+// .toContain() → arrays/strings.
+// .toThrow() → error testing.
